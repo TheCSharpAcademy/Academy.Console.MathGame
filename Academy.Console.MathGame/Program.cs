e1f71503c647f904ad76a4fca842ccce513d1579
TheCSharpAcademy <thecsharpacademy@gmail.com>
2022-07-26 07:32:17 +1000

    Update Program.cs

--- Academy.Console.MathGame/Program.cs
+++ Academy.Console.MathGame/Program.cs
@@ -1,3 +1,8 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("These are the most common data types:");
+Console.WriteLine(1 + " string , Example: " + "Pablo");
+Console.WriteLine(2 + " char , Example: " + 'P');
+Console.WriteLine(3 + " int , Example: " + 2022);
+Console.WriteLine(4 + " decimal , Example: " + 1.85m);
+Console.WriteLine(5 + " bool  , Example: " + true);
 Console.ReadLine();
+
