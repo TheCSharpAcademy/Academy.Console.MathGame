1034d78952ab061bf74d350a328282cc13a0d32a
TheCSharpAcademy <thecsharpacademy@gmail.com>
2022-07-31 07:34:39 +1000

    Refactoring

---
+++ Academy.Console.MathGame/Helpers.cs
@@ -0,0 +1,46 @@
+﻿namespace MyFirstProgram
+{
+    public class Helpers
+    {
+        public static List<string> games = new List<string>();
+
+        public static int[] GetDivisionNumbers()
+        {
+            var random = new Random();
+            var firstNumber = random.Next(1, 99);
+            var secondNumber = random.Next(1, 99);
+
+            var result = new int[2];
+
+            while (firstNumber % secondNumber != 0)
+            {
+                firstNumber = random.Next(1, 99);
+                secondNumber = random.Next(1, 99);
+            }
+
+            result[0] = firstNumber;
+            result[1] = secondNumber;
+
+            return result;
+        }
+
+        public static void AddToHistory(int gameScore, string gameType)
+        {
+            games.Add($"{DateTime.Now} - {gameType}: {gameScore} pts");
+        }
+
+        public static void PrintGames()
+        {
+            Console.Clear();
+            Console.WriteLine("Games History");
+            Console.WriteLine("---------------------------");
+            foreach (var game in games)
+            {
+                Console.WriteLine(game);
+            }
+            Console.WriteLine("---------------------------\n");
+            Console.WriteLine("Press any key to return to Main Menu");
+            Console.ReadLine();
+        }
+    }
+}
