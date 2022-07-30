fa66973b64a3c97a5c13a7bf3933f532ce3ca7b2
TheCSharpAcademy <thecsharpacademy@gmail.com>
2022-07-31 09:06:14 +1000

    enums

---
+++ Academy.Console.MathGame/Helpers.cs
@@ -0,0 +1,53 @@
+﻿using Academy.Console.MathGame.Models;
+
+namespace MyFirstProgram
+{
+    internal class Helpers
+    {
+        internal static List<Game> games = new List<Game>();
+
+        internal static int[] GetDivisionNumbers()
+        {
+            var random = new Random();
+            var firstNumber = random.Next(1, 99);
+            var secondNumber = random.Next(1, 99);
+
+            var result = new int[2];
+
+            while (firstNumber % secondNumber != 0)
+            {
+                firstNumber = random.Next(1, 99);
+                secondNumber = random.Next(1, 99);
+            }
+
+            result[0] = firstNumber;
+            result[1] = secondNumber;
+
+            return result;
+        }
+
+        internal static void AddToHistory(int gameScore, GameType gameType)
+        {
+            games.Add(new Game
+            {
+                Date = DateTime.Now,
+                Score = gameScore,
+                Type = gameType
+            });
+        }
+
+        internal static void PrintGames()
+        {
+            Console.Clear();
+            Console.WriteLine("Games History");
+            Console.WriteLine("---------------------------");
+            foreach (var game in games)
+            {
+                Console.WriteLine($"{game.Date} - {game.Type}: {game.Score}pts");
+            }
+            Console.WriteLine("---------------------------\n");
+            Console.WriteLine("Press any key to return to Main Menu");
+            Console.ReadLine();
+        }
+    }
+}
