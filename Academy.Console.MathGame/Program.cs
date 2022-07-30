9e1cacea018f5ed17fc1937610e7b6fa8fde91df
TheCSharpAcademy <thecsharpacademy@gmail.com>
2022-07-31 07:16:28 +1000

    Update Program.cs

--- Academy.Console.MathGame/Program.cs
+++ Academy.Console.MathGame/Program.cs
@@ -1,5 +1,7 @@
 ﻿var date = DateTime.UtcNow;
 
+var games = new List<string>();
+
 string name = GetName();
 
 Menu(name);
@@ -22,18 +24,23 @@ void Menu(string name)
     do
     {
         Console.Clear();
-        Console.WriteLine(@$"What game would you like to play today? Choose from the options below:
-                             A - Addition
-                             S - Subtraction
-                             M - Multiplication
-                             D - Division
-                             Q - Quit the program");
+        Console.WriteLine(@$"
+What game would you like to play today? Choose from the options below:
+V - View Previous Games
+A - Addition
+S - Subtraction
+M - Multiplication
+D - Division
+Q - Quit the program");
         Console.WriteLine("---------------------------------------------");
 
         var gameSelected = Console.ReadLine();
 
         switch (gameSelected.Trim().ToLower())
         {
+            case "v":
+                GetGames();
+                break;
             case "a":
                 AdditionGame("Addition game");
                 break;
@@ -57,6 +64,20 @@ void Menu(string name)
     } while (isGameOn);
 }
 
+void GetGames()
+{
+    Console.Clear();
+    Console.WriteLine("Games History");
+    Console.WriteLine("---------------------------");
+    foreach (var game in games)
+    {
+        Console.WriteLine(game);
+    }
+    Console.WriteLine("---------------------------\n");
+    Console.WriteLine("Press any key to return to Main Menu");
+    Console.ReadLine();
+}
+
 void DivisionGame(string message)
 {
     var score = 0;
@@ -75,28 +96,24 @@ void DivisionGame(string message)
 
         if (int.Parse(result) == firstNumber / secondNumber)
         {
-            Console.WriteLine($"Your answer was correct. Type any key for the next question.");
+            Console.WriteLine("Your answer was correct! Type any key for the next question");
             score++;
             Console.ReadLine();
         }
         else
         {
-            Console.WriteLine($"Your answer was incorrect.");
+            Console.WriteLine("Your answer was incorrect. Type any key for the next question");
             Console.ReadLine();
         }
 
-        if (i == 4)
-        {
-            Console.WriteLine($"Game over. Your final score is {score}. Press any key to go back to the main menu.");
-            Console.ReadLine();
-        }
+        if (i == 4) Console.WriteLine($"Game over. Your final score is {score}. Press any key to go back to the main menu.");
     }
+
+    AddToHistory(score, "Division");
 }
 
 void MultiplicationGame(string message)
 {
-    Console.WriteLine(message);
-
     var random = new Random();
     var score = 0;
 
@@ -116,28 +133,24 @@ void MultiplicationGame(string message)
 
         if (int.Parse(result) == firstNumber * secondNumber)
         {
-            Console.WriteLine($"Your answer was correct. Type any key for the next question.");
+            Console.WriteLine("Your answer was correct! Type any key for the next question");
             score++;
             Console.ReadLine();
         }
         else
         {
-            Console.WriteLine($"Your answer was incorrect.");
+            Console.WriteLine("Your answer was incorrect. Type any key for the next question");
             Console.ReadLine();
         }
 
-        if (i == 4)
-        {
-            Console.WriteLine($"Game over. Your final score is {score}. Press any key to go back to the main menu.");
-            Console.ReadLine();
-        }
+        if (i == 4) Console.WriteLine($"Game over. Your final score is {score}. Press any key to go back to the main menu.");
     }
+
+    AddToHistory(score, "Multiplication");
 }
 
 void SubtractionGame(string message)
 {
-    Console.WriteLine(message);
-
     var random = new Random();
     var score = 0;
 
@@ -157,28 +170,24 @@ void SubtractionGame(string message)
 
         if (int.Parse(result) == firstNumber - secondNumber)
         {
-            Console.WriteLine($"Your answer was correct. Type any key for the next question.");
+            Console.WriteLine("Your answer was correct! Type any key for the next question");
             score++;
             Console.ReadLine();
         }
         else
         {
-            Console.WriteLine($"Your answer was incorrect.");
+            Console.WriteLine("Your answer was incorrect. Type any key for the next question");
             Console.ReadLine();
         }
 
-        if (i == 4)
-        {
-            Console.WriteLine($"Game over. Your final score is {score}. Press any key to go back to the main menu.");
-            Console.ReadLine();
-        }
+        if (i == 4) Console.WriteLine($"Game over. Your final score is {score}. Press any key to go back to the main menu.");
     }
+
+    AddToHistory(score, "Subtraction");
 }
 
 void AdditionGame(string message)
 {
-    Console.WriteLine(message);
-
     var random = new Random();
     var score = 0;
 
@@ -190,21 +199,21 @@ void AdditionGame(string message)
         Console.Clear();
         Console.WriteLine(message);
 
-        firstNumber = random.Next(1,9);
-        secondNumber = random.Next(1,9);
+        firstNumber = random.Next(1, 9);
+        secondNumber = random.Next(1, 9);
 
         Console.WriteLine($"{firstNumber} + {secondNumber}");
         var result = Console.ReadLine();
 
         if (int.Parse(result) == firstNumber + secondNumber)
         {
-            Console.WriteLine($"Your answer was correct. Type any key for the next question.");
+            Console.WriteLine("Your answer was correct! Type any key for the next question");
             score++;
             Console.ReadLine();
         }
         else
         {
-            Console.WriteLine($"Your answer was incorrect.");
+            Console.WriteLine("Your answer was incorrect. Type any key for the next question");
             Console.ReadLine();
         }
 
@@ -214,13 +223,20 @@ void AdditionGame(string message)
             Console.ReadLine();
         }
     }
+
+    AddToHistory(score, "Addition");
+}
+
+void AddToHistory(int gameScore, string gameType)
+{
+    games.Add($"{DateTime.Now} - {gameType}: {gameScore} pts");
 }
 
 int[] GetDivisionNumbers()
 {
     var random = new Random();
-    var firstNumber = random.Next(0, 99);
-    var secondNumber = random.Next(0, 99);
+    var firstNumber = random.Next(1, 99);
+    var secondNumber = random.Next(1, 99);
 
     var result = new int[2];
 
